c9557cf8195506142245eae516a4c88b9861b2cb
Arunkumar Rajbhar <arunkbr@gitl.com>
2022-02-23 16:46:34 +0530

    Added API URL

--- src/services/upload-service.js
+++ src/services/upload-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = process.env.UPLOAD_API_URL;
-const MULTIPLE = "http://localhost:5000/excelupload/multiple";
+const MULTIPLE = process.env.REACT_APP_UPLOAD_MULTIPLE_API_URL;
 
 export async function getRecords(){
     return fetch(API_URL).then(response=>response.json());
